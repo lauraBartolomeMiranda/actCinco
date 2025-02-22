0c63662ce9b357824485687b62575dd8b7c32c36
Laura <laura@info.com>
2025-02-22 10:46:50 +0100

    arrays iniciales se ven en consola

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { INoticia } from '../../interfaces/inoticia.interface';
 
 @Component({
   selector: 'app-blog',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  noticias: INoticia[] = []; //array tipado interfaz
 
+  ngOnInit(){
+    this.noticias = [
+      {
+        titulo: 'Lanzamiento de Sual!',
+        descripcion: 'La nueva marca de accesorios y complementos.',
+        fecha: '2023-06-17'
+      },
+      {
+        titulo: '¿Es sual la nueva competencia de Parfois?',
+        descripcion: 'Un análisis sobre la popularidad de sual...',
+        fecha: '2024-01-12'
+      }
+    ];
+    console.log('array noticias:', this.noticias)
+  }
+  
 }
